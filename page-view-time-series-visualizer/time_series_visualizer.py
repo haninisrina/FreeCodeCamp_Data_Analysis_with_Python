7fa5a86668267d9b7a433b7b42c04a2804c474eb
Hani Nisrina <hnisrina28@gmail.com>
2025-05-08 22:56:32 +0700

    Update beberapa file

--- page-view-time-series-visualizer/time_series_visualizer.py
+++ page-view-time-series-visualizer/time_series_visualizer.py
@@ -5,18 +5,22 @@ from pandas.plotting import register_matplotlib_converters
 register_matplotlib_converters()
 
 # Import data (Make sure to parse dates. Consider setting index column to 'date'.)
-df = None
+df = pd.read_csv('fcc-forum-pageviews.csv')
+df["date"] = pd.to_datetime(df["date"])
+df.set_index("date", inplace = True)
 
 # Clean data
-df = None
+df = df[(df['value']>df['value'].quantile(0.025)) &
+       ((df['value']<df['value'].quantile(0.975)))]
 
 
 def draw_line_plot():
     # Draw line plot
-
-
-
-
+    fig = plt.figure(figsize=(18,6))
+    plt.plot(df.index, df['value'])
+    plt.title('Daily freeCodeCamp Forum Page Views 5/2016-12/2019')
+    plt.xlabel('Date')
+    plt.ylabel('Page Views')
 
     # Save image and return fig (don't change this part)
     fig.savefig('line_plot.png')
@@ -24,13 +28,27 @@ def draw_line_plot():
 
 def draw_bar_plot():
     # Copy and modify data for monthly bar plot
-    df_bar = None
+    df_bar = df.copy(deep=True)
+    df_bar['year'] = pd.DatetimeIndex(df_bar.index).year
+    df_bar['month'] = pd.DatetimeIndex(df_bar.index).month
+
+    df_bar = df_bar.groupby(['year', 'month'])['value'].mean().unstack()
+
+    months = ["January", "February", "March", "April", "May", "June", "July", "August",
+              "September", "October", "November", "December"]
+  
 
     # Draw bar plot
+    fig = df_bar.plot.bar()
+    fig.legend(months, title='Months', prop={'size': 12})
+    plt.xlabel("Years")
+    plt.ylabel("Average Page Views")
+    plt.tight_layout()
+    fig = fig.figure
 
-
-
-
+    # Save image and return fig (don't change this part)
+    fig.savefig('bar_plot.png')
+    return fig
 
     # Save image and return fig (don't change this part)
     fig.savefig('bar_plot.png')
@@ -42,11 +60,14 @@ def draw_box_plot():
     df_box.reset_index(inplace=True)
     df_box['year'] = [d.year for d in df_box.date]
     df_box['month'] = [d.strftime('%b') for d in df_box.date]
+    df_box['monthnum'] = df_box['date'].dt.month
+    df_box = df_box.sort_values('monthnum')
 
     # Draw box plots (using Seaborn)
+    fig, ax = plt.subplots(ncols=2, figsize=(15,5))
 
-
-
+    sns.boxplot(ax=ax[0], x='year', y='value', data=df_box).set(xlabel="Year", ylabel="Page Views", title="Year-wise Box Plot (Trend)")
+    sns.boxplot(ax=ax[1], x='month', y='value', data=df_box ).set(xlabel="Month", ylabel="Page Views", title="Month-wise Box Plot (Seasonality)")
 
 
     # Save image and return fig (don't change this part)
--- sea-level-predictor/sea_level_predictor.py
+++ sea-level-predictor/sea_level_predictor.py
@@ -1,21 +1,31 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.stats import linregress
+import numpy as np
 
 def draw_plot():
     # Read data from file
-
+    df = pd.read_csv('epa-sea-level.csv')
 
     # Create scatter plot
-
-
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ax.scatter(df["Year"], df["CSIRO Adjusted Sea Level"])
     # Create first line of best fit
-
+    res1 = linregress(df['Year'], df['CSIRO Adjusted Sea Level'])
+    x1 = np.arange(df['Year'].min(),2051,1)
+    y1 = res1.intercept + res1.slope*x1
+    plt.plot(x1,y1,color='firebrick')
 
     # Create second line of best fit
-
+    x_ = range(2000, df["Year"].iloc[-1]+1, 1)
+    slope, intercept, r_value, p_value, std_err = linregress(x_, df[-len(x_):]["CSIRO Adjusted Sea Level"])
+    x2 = range(2000, 2051, 1)
+    plt.plot(x2, intercept + slope*x2, 'r', label='fitted line 2')
 
     # Add labels and title
+    plt.title("Rise in Sea Level")
+    plt.xlabel("Year")
+    plt.ylabel("Sea Level (inches)")
 
     
     # Save plot and return data for testing (DO NOT MODIFY)
